4d2b211f3eb1c0ac510008babdb97649113ff23f
Bart Kampers <github@bartkampers.nl>
2015-08-23 13:32:28 +0200

    MessengerTest added.

---
+++ test/randd/motormanagement/communication/MessengerTest.java
@@ -0,0 +1,167 @@
+/*
+** Copyright © Bart Kampers
+*/
+
+package randd.motormanagement.communication;
+
+import bka.communication.*;
+import org.json.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.*;
+import static org.mockito.Mockito.when;
+
+
+public class MessengerTest {
+
+    private static Transporter transporter;
+    private Messenger messenger;
+
+
+    public MessengerTest() {
+    }
+
+
+    @BeforeClass
+    public static void setUpClass() throws InterruptedException, JSONException {
+        transporter = Mockito.mock(Transporter.class);
+        when(transporter.getName()).thenReturn("TransporterMock");
+    }
+
+
+    @AfterClass
+    public static void tearDownClass() {
+        transporter = null;
+    }
+
+
+    @Before
+    public void setUp() {
+        messenger = new Messenger(transporter);
+    }
+
+
+    @After
+    public void tearDown() {
+        messenger = null;
+    }
+
+
+    /**
+     * Test receiving notification.
+     * @throws bka.communication.ChannelException
+     * @throws java.lang.InterruptedException
+     * @throws org.json.JSONException
+     */
+    @Test
+    public void testNotification() throws ChannelException, InterruptedException, JSONException {
+        JSONObject notification = new JSONObject();
+        notification.put("Notification", "Mocked notification");
+        MessengerListener listener = new MessengerListener();
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        messenger.setListener(listener);
+        messenger.open();
+        when(transporter.nextReceivedObject()).thenReturn(notification);
+        Thread.sleep(5);
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        messenger.close();
+        assertEquals(notification, listener.receivedNotification);
+    }
+
+
+    /**
+     * Test receiving response for sent message
+     * @throws bka.communication.ChannelException
+     * @throws java.lang.InterruptedException
+     * @throws org.json.JSONException
+     */
+    @Test
+    public void testResponse() throws ChannelException, InterruptedException, JSONException {
+        JSONObject message = new JSONObject();
+        message.put("Message", "Request");
+        message.put("Subject", "Test");
+        JSONObject response = new JSONObject();
+        response.put("Response", "Request");
+        response.put("Subject", "Test");
+        MessengerListener listener = new MessengerListener();
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        messenger.setListener(listener);
+        messenger.open();
+        messenger.send(message);
+        when(transporter.nextReceivedObject()).thenReturn(response);
+        Thread.sleep(5);
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        messenger.close();
+        assertEquals(message, listener.receivedMessage);
+        assertEquals(response, listener.receivedResponse);
+    }
+
+
+    /**
+     * Test notification not received when messenger not opened.
+     * @throws java.lang.InterruptedException
+     * @throws org.json.JSONException
+     */
+    @Test
+    public void testNotOpen() throws InterruptedException, JSONException  {
+        JSONObject notification = new JSONObject();
+        notification.put("Notification", "Mocked notification");
+        MessengerListener listener = new MessengerListener();
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        messenger.setListener(listener);
+        when(transporter.nextReceivedObject()).thenReturn(notification);
+        Thread.sleep(5);
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        assertNull(listener.receivedNotification);
+    }
+
+
+    /**
+     * Test notification not received when messenger after close.
+     * @throws bka.communication.ChannelException
+     * @throws java.lang.InterruptedException
+     * @throws org.json.JSONException
+     */
+    @Test
+    public void testClose() throws ChannelException, InterruptedException, JSONException  {
+        JSONObject notification = new JSONObject();
+        notification.put("Notification", "Mock");
+        MessengerListener listener = new MessengerListener();
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        messenger.setListener(listener);
+        messenger.open();
+        messenger.close();
+        when(transporter.nextReceivedObject()).thenReturn(notification);
+        Thread.sleep(5);
+        when(transporter.nextReceivedObject()).thenReturn(EMPTY);
+        assertNull(listener.receivedNotification);
+    }
+
+
+    private class MessengerListener implements Messenger.Listener {
+
+        @Override
+        public void notifyMessage(JSONObject message) {
+            receivedNotification = message;
+        }
+
+        @Override
+        public void notifyResponse(JSONObject message, JSONObject response) {
+            receivedMessage = message;
+            receivedResponse = response;
+        }
+
+        JSONObject receivedNotification;
+        JSONObject receivedMessage;
+        JSONObject receivedResponse;
+
+    }
+
+
+    private static final JSONObject EMPTY = new JSONObject();
+ 
+}
